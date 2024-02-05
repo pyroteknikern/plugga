a8dae3fcfd84c5c4fc534b31408257e7ed0c7836
pyroteknikern <118835712+pyroteknikern@users.noreply.github.com>
2024-02-05 20:12:31 +0100

    consts

--- app/constants.py
+++ app/constants.py
@@ -15,7 +15,5 @@ WEEK_RESET_DAY = 0
 MIN_TIME_DAY = 90
 MIN_TIME_WEEK = 600
 
-sqlWHERE__users__challage_accepted__o = "WHERE challange_accepted=1"
 FORMAT = "%d-%m-%Y"
 SWE_TIME = pytz.timezone("Europe/Stockholm")
-
